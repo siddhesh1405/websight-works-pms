76011f72a1604aec44d2a298e92e9a191314e1d7
Siddhesh Kamble <siddheshkamble@Siddheshs-MacBook-Air.local>
2026-03-18 01:38:53 +0000

    Update browser title and favicon branding

--- pms-frontend/src/main.jsx
+++ pms-frontend/src/main.jsx
@@ -2,6 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import brandLogo from "./assets/websight_works_logo.jpeg";
+
+document.title = "Websight Works";
+
+const faviconLink =
+  document.querySelector("link[rel='icon']") ||
+  Object.assign(document.createElement("link"), { rel: "icon" });
+
+faviconLink.href = brandLogo;
+faviconLink.type = "image/jpeg";
+
+if (!faviconLink.parentNode) {
+  document.head.appendChild(faviconLink);
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
